18cb865c364f38cb76aaafb84b982c41337ac6f1
aasthakumar <31899835+aasthakumar@users.noreply.github.com>
2018-03-09 12:38:17 -0800

    updated to include threading instead of alarm

--- lab3/client.py
+++ lab3/client.py
@@ -1,60 +1,36 @@
 import zmq
 import sys
-import time
-from signal import signal, alarm, SIGALRM
+from threading import Thread
 
-signal(SIGALRM, lambda x,y:(1/0))
+client_name = sys.argv[1]
 
+def sending():
+    context = zmq.Context()
+    sock = context.socket(zmq.REQ)
+    sock.connect("tcp://127.0.0.1:5677")
 
-class Client():
-    def __init__(self, name):
-        self.context = zmq.Context()        
-        self.pubname = self.context.socket(zmq.PUSH)
-        self.pubname.connect("tcp://127.0.0.1:5677")      
-        
-        self.sock = self.context.socket(zmq.SUB)
-        self.sock.setsockopt(zmq.SUBSCRIBE,b'')
-        self.sock.connect("tcp://127.0.0.1:5678")
+    while 1:
+        val = str("[" + client_name + "]:" + input("[" + client_name + "]>"))
+        #print(val)
+        sock.send_string(val)
+        mess= (sock.recv().decode())
 
-        self.clientname = name
+def receiving():
+    context = zmq.Context()
+    sock = context.socket(zmq.SUB)
+    sock.setsockopt_string(zmq.SUBSCRIBE, "")
+    sock.connect("tcp://127.0.0.1:5678")
 
-    def initiateClient(self,name):
-        self.pubname.send_string(name)
-        self.message = self.sock.recv()
-        self.message =  self.message.decode('utf-8')
-        #print(self.message)
-        
-    
-    def callserver(self,name):
-        while 1:
-            try: 
-                while True:
-                    try:                       
-                        alarm(10)
-                        val = input("\n[" + self.clientname + "] >")
-                        self.initiateClient("[" + self.clientname + "] >" + val) 
-                    except ZeroDivisionError:
-                        self.message = self.sock.recv()
-                        self.message =  self.message.decode('utf-8')
-                        try:
-                            key, value = self.message.split(":")    
-                        except ValueError:
-                            print(self.message) 
-                        else:
-                            if key.strip() != "[" + self.clientname +"]":
-                                print(self.message) 
-                        
-                        
-                            #client_inst.callserver(val)  
-            except KeyboardInterrupt:
-                break                           
-        
-    
-def callClient(name):
-    client_inst = Client(name)
-    client_inst.initiateClient(name)
-    client_inst.callserver(name)
-    
-if __name__ == '__main__':
-    cmdargs = str(sys.argv[1])
-    callClient(cmdargs)
+    while True:
+        message= sock.recv().decode()
+        if message.find("[" + client_name + "]:") != 0:
+            print("\n" + message + "\n[" + client_name + "]>", end="")
+            
+
+print("User[" + client_name + "] Connected to the chat server.")
+
+output = (Thread(target=sending, args=( )))
+output.start()
+
+receive = (Thread(target=receiving, args=( )))
+receive.start()
